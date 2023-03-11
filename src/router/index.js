8aa1c3ddc4b01b3fe49c3afc44f176686bafa20c
Fabri <fabricio@5e.cr>
2023-03-11 12:41:54 -0600

    Creacion e indexado de paginas a utilizar  en el vue router

--- src/router/index.js
+++ src/router/index.js
@@ -10,9 +10,19 @@ const routes = [
     component: () => import('../pages/Home.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('../pages/About.vue')
+    path: '/tasks',
+    name: 'task',
+    component: () => import('../pages/tasks/Index.vue')
+  },
+  {
+    path: '/tasks/create',
+    name: 'create task',
+    component: () => import('../pages/tasks/Create.vue')
+  },
+  {
+    path: '/notifications',
+    name: 'notificactions',
+    component: () => import('../pages/notificactions/Index.vue')
   }
 ]
 
