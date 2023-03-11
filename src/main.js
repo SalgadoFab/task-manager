e431ae3a905b31e701242a3afcbabd01575a94e6
Fabri <fabricio@5e.cr>
2023-03-10 20:22:51 -0600

    nuevas librerias añadidas: Vuex, Vuesax, Material Icons y Axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import Vuesax from 'vuesax';
+import router from './router';
+import App from './App.vue';
 
 import './assets/scss/app.scss'
+import 'vuesax/dist/vuesax.css'; 
 
-Vue.config.productionTip = false
+Vue.use(Vuesax);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
