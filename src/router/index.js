57a8e37406a935f874d2a84922434882f4e0a7c3
Fabri <fabricio@5e.cr>
2023-03-12 09:35:47 -0600

    Creacion de la ruta de login

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import HomePage from '../pages/home/Index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: () => import('../pages/home/Index.vue')
+    name: 'Home',
+    component: HomePage,
+    //meta: { requiresAuth: true }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../pages/user/Login.vue')
   },
   {
     path: '/tasks',
-    name: 'task',
+    name: 'Tasks',
     component: () => import('../pages/tasks/Index.vue')
   },
   {
     path: '/tasks/create',
-    name: 'create task',
+    name: 'Create task',
     component: () => import('../pages/tasks/Create.vue')
   },
   {
     path: '/notifications',
-    name: 'notificactions',
+    name: 'Notificactions',
     component: () => import('../pages/notifications/Index.vue')
   }
 ]
