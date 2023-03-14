ab095f397e099d7a42abd158e7fc7bbd420128c6
Fabri <fabricio@5e.cr>
2023-03-13 20:32:55 -0600

    Mensaje de bienvenida del usuario en home

--- src/pages/home/Index.vue
+++ src/pages/home/Index.vue
@@ -2,32 +2,39 @@
   <div class="home-container padding-app page-container">
     <div class="wrapper-info">
       <div class="content">
-        <vs-tabs  :color="colorx" alignment="center">
+        <vs-tabs :color="colorx" alignment="center">
           <vs-tab @click="colorx = 'success'" label="Información">
             <div>
-              <InformationComponent/>
+              <InformationComponent />
             </div>
           </vs-tab>
           <vs-tab @click="colorx = 'primary'" label="Curso">
             <div>
-              <SubjectComponent/>
+              <SubjectComponent />
             </div>
           </vs-tab>
           <vs-tab @click="colorx = 'warning'" label="Desarrollador">
             <div>
-              <DeveloperComponent/>
+              <DeveloperComponent />
             </div>
           </vs-tab>
         </vs-tabs>
       </div>
     </div>
     <div class="wrapper-action">
-      <vs-button color="success" to="/tasks/create" type="gradient" icon="drive_file_rename_outline">Crear nueva tarea</vs-button>
+      <vs-button
+        color="success"
+        to="/tasks/create"
+        type="gradient"
+        icon="drive_file_rename_outline"
+        >Crear nueva tarea</vs-button
+      >
     </div>
   </div>
 </template>
 
 <script>
+//Componentes
 import InformationComponent from "@/components/home/Information.vue";
 import SubjectComponent from "@/components/home/Subject.vue";
 import DeveloperComponent from "@/components/home/Developer.vue";
@@ -37,10 +44,32 @@ export default {
   components: {
     InformationComponent,
     SubjectComponent,
-    DeveloperComponent
+    DeveloperComponent,
   },
-  data:()=>({
-    colorx:'success'
+  data: () => ({
+    colorx: "success",
+    showWelcomeMessage: true
   }),
+  methods: {
+    wellcomeMessage() {
+      this.$vs.notify({
+        color:'success',
+        title:'Bienvenido!',
+        text:'Que alegria tenerte de vuelta'
+      })
+    }, 
+  },
+  created() {
+  // Comprobamos si la bandera ya ha sido establecida en localStorage
+  const flag = localStorage.getItem('showWelcomeMessage')
+  if (flag) {
+    // Si la bandera no ha sido establecida, mostramos el mensaje de bienvenida y establecemos la bandera en localStorage
+    this.showWelcomeMessage = false
+    this.$nextTick(() => {
+      this.wellcomeMessage()
+      localStorage.setItem('showWelcomeMessage', false)
+    })
+  }
+},
 };
 </script>
