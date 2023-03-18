c8687ed180ed735933d3366cae62732c31e282cf
Fabri <fabricio@5e.cr>
2023-03-17 19:42:05 -0600

    Inicio del formulario de registro de tareas

--- src/pages/tasks/Create.vue
+++ src/pages/tasks/Create.vue
@@ -1,6 +1,21 @@
 <template>
-    <div class="create-task-container padding-app">
-      <h1>Crear Tarea</h1>
-    </div>
-  </template>
-    
+  <div class="create-task-container padding-app">
+    <h1>Crear Tarea</h1>
+    <Register/>
+  </div>
+</template>
+<script>
+//Componentes
+import Register from "@/components/tasks/FormRegister.vue";
+
+export default {
+  name: "CreateTaskPage",
+  components: {
+    Register,
+  },
+  data: () => ({
+    colorx: "success",
+    showWelcomeMessage: true,
+  })
+};
+</script>
